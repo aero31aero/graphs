35b83f883cb9560e1d86aeea1f0683eda0d3e8ce
Rohitt Vashishtha <aero31aero@gmail.com>
2017-04-15 13:39:26 +0530

    Add weights and algorithm. Starting work on representation.

--- index.js
+++ index.js
@@ -8,13 +8,17 @@ window.onload = function(e){
         id: node.name
       }
       data.nodes.push(nodeobj);
-      node.adjList.forEach(function(link){
+      node.adjList.forEach(function(link,count){
         if(!link){
+          return;
+        }
+        if(node.name < link.name){
           return;
         }
         linkobj = {
           source: node.name,
-          target: link.name
+          target: link.name,
+          weight: node.weight[count]
         }
         data.links.push(linkobj);
       });
@@ -27,7 +31,7 @@ window.onload = function(e){
     var links = [];
     var shouldMakeEdge = function(){
       var random = Math.random();
-      if(random>0.2){
+      if(random>0.5){
         return true;
       }
       return false;
@@ -39,9 +43,11 @@ window.onload = function(e){
       nodes.forEach(function(target){
         if(source < target && target - source < 5){
           if(shouldMakeEdge()==true){
+            var weight = 1+Math.floor(Math.random()*20);
             links.push({
               source: source,
-              target: target
+              target: target,
+              weight: weight
             });
           }
         }
@@ -61,24 +67,62 @@ window.onload = function(e){
     })
     data.links.forEach(function(link){
       // console.log(link);
-      nodes[link.source].addEdge(nodes[link.target]);
+      nodes[link.source].addEdge(nodes[link.target],link.weight);
+      nodes[link.target].addEdge(nodes[link.source],link.weight);
     })
     return graph;
   }
 
+  var highlightPath = function(path,instance,source,target){
+    var route = [];
+    var foundroot = false;
+    route.push(target);
+    var maxtries = 15;
+    while(!foundroot){
+      target = path[target];
+      route.push(target);
+      if(target==source){
+        foundroot = true;
+      }
+      maxtries--;
+      if(maxtries == 0){
+        foundroot = true;
+      }
+      console.log(route);
+    }
+
+    var order = instance.graph.nodes.length;
+    var size = instance.graph.edges.length;
+
+    var id = Math.floor(Math.random() * order);
+
+    var adjacent = instance.graph.getAdjacentNodes({ id: id });
+    var v = adjacent[0].id;
+    instance.selector.traverseAllEdgesBetween({ source: id, target: v });
+  }
+  window.highlightPath = highlightPath;
+
   var main = function(){
-    var graph = makeGraph(makeRandomGraph(10));
+    var graph = makeGraph(makeRandomGraph(15));
     var data = getVisualData(graph);
     var graphholder = document.getElementById("graph-container");
+    console.log("width",graphholder.width);
+    console.log("height",graphholder.height);
+    console.log(graphholder);
     var instance = greuler({
       target: '#graph',
-      width: graphholder.width,
-      height: graphholder.height,
+      width: 1500,
+      height: 600,
       data: data
     });
     console.log(instance);
     instance.update();
+    var nodes=graph.getAllNodes();
+    var path=dijkstra(graph,nodes[0],nodes[10]);
+    console.log("Distance",path);
+    highlightPath(path,instance,0,10);
+    return instance;
   }
 
-  main();
+  window.instance = main();
 };
